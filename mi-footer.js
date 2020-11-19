31a95f18cf12da1224b6bae2e5a6aeffa0bac05f
morjimmonsepwa <74696063+morjimmonsepwa@users.noreply.github.com>
2020-11-18 22:28:50 -0600

    Delete mi-footer.js

--- mi-footer.js
+++
@@ -1,7 +0,0 @@
-class MiFooter extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = /* html */
-      `Copyright &copy; Monserrat Morales Jimenez`;
-  }
-}
-customElements.define("mi-footer", MiFooter);
